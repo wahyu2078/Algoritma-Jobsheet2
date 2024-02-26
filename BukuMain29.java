c1370b5aa850628276e1b1734eb78d523ec982c5
Wahyu <wahyutptp20@gmail.com>
2024-02-26 20:08:32 +0700

    Buku Mahasiswa

--- BukuMain29.java
+++ BukuMain29.java
@@ -12,5 +12,12 @@ public class BukuMain29 {
         Buku29 bk2 = new Buku29("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
+
+        Buku29 bukuWahyu = new Buku29("Judul Buku Wahyu", "Pengarang Wahyu", 200, 50, 75000);
+        bukuWahyu.tampilInformasi();
+        int jumlahTerjual = 8;
+        int hargaTotal = bukuWahyu.hitungHargaTotal(jumlahTerjual);
+        double diskon = bukuWahyu.hitungDiskon(hargaTotal);
+        double hargaBayar = bukuWahyu.hitungHargaBayar(jumlahTerjual);
     }
 }
